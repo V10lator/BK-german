a0157b7e4e30bb7e3ffa9412537fcffa15b59266
Thomas Rohloff <v10lator@myway.de>
2026-02-14 02:25:09 +0100

    BK-german initial commit
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

---
+++ include/dialogs.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "functions.h"
+
+#define ASSETS_SIZE 912
+
+extern const u8 *asset_data[ASSETS_SIZE];
+extern const u32 asset_name[ASSETS_SIZE];
---
+++ src/main.c
@@ -0,0 +1,27 @@
+#include "modding.h"
+#include "functions.h"
+#include "variables.h"
+#include "recomputils.h"
+#include "recompconfig.h"
+
+#include "dialogs.h"
+
+RECOMP_IMPORT("bk_recomp_asset_expansion_pak", void bk_recomp_aep_register_replacement(enum asset_e asset_id, void *asset_data));
+RECOMP_IMPORT("bk_recomp_asset_expansion_pak", void bk_recomp_aep_unregister_replacement(enum asset_e asset_id));
+
+RECOMP_CALLBACK("*", recomp_on_init)
+void onInit()
+{
+    for(int i = 0; i < ASSETS_SIZE; i++)
+        bk_recomp_aep_register_replacement(asset_name[i], (void *)asset_data[i]);
+
+    recomp_printf("German mod: %i assets loaded!\n", ASSETS_SIZE);
+}
+/* TODO
+RECOMP_CALLBACK("*", recomp_exit)
+void on_deinit()
+{
+    for(int i = 0; i < ASSETS_SIZE; i++)
+        bk_recomp_aep_unregister_replacement(asset_name[i]);
+}
+*/
